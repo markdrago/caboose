daa605525eb94bba0d6983cf4f1af28fd9e2cbbf
Mark Drago <mdrago@yodle.com>
2011-02-18 08:42:45 -0500

    initial commit of code to count lines of code in a project over time

---
+++ gather-code-changes.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#set default parameters
+directory='.'
+group='monthly'
+metric='lines'
+outfile='code-changes.out'
+start_rev='0'
+
+#get options from the command line
+while getopts "d:g:o:m:s:" argparsed; do
+    if [ "$argparsed" == "d" ]; then
+        directory=$OPTARG
+    elif [ "$argparsed" == "g" ]; then
+        group=$OPTARG
+    elif [ "$argparsed" == "o" ]; then
+        outfile=$OPTARG
+    elif [ "$argparsed" == "m" ]; then
+        metric=$OPTARG
+    elif [ "$argparsed" == "s" ]; then
+        start_rev=$OPTARG
+    fi
+done
+
+#absolute-ify the outfile path
+if [ "${outfile:0:1}" != "/" ]; then
+    wd=`pwd`
+    outfile="${wd}/${outfile}"
+fi
+
+#empty output file
+echo -n > $outfile
+
+#setup grouping
+group_seconds=$((60*60*24*30))
+
+pushd $directory &> /dev/null
+
+#get date of oldest commit in repo and today's date
+moment=`hg log -r $start_rev --template "{date}" | cut -f1 -d.`
+now=`date +%s`
+
+#loop over revisions, jumping by group_seconds each time
+while [ $moment -le $now ]; do
+    #update to the revision just before the specified date
+    hg up -d "<$moment 0"
+
+    #count the number of lines in all files in this directory
+    numlines=`find . -type f -name '*.java' -print0 | wc -l --files0-from=- | tail -n 1 | awk '{print $1}'`
+    
+    #produce output in outfile
+    pretty_date=`date +%Y-%m --date=@$moment`
+    echo "${pretty_date},${numlines}" >> $outfile
+    
+    #prepare for next loop iteration by adding group_seconds
+    moment=$(($moment+$group_seconds))
+done
+
+popd &> /dev/null
+
