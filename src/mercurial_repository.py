834772b018ac965523c1eb32e9a5feb11b62041a
Mark Drago <mdrago@yodle.com>
2011-03-01 18:51:49 -0500

    make mercurial_repository a lot nicer

--- src/mercurial_repository.py
+++ src/mercurial_repository.py
@@ -3,13 +3,14 @@ from mercurial import hg, ui, commands
 from repository import Repository
 
 class MercurialRepository(Repository):
-    def switch_to_revision(self, rev, directory='.'):
-        hg_ui = ui.ui()
-        repo = hg.repository(hg_ui, directory)
-        commands.update(hg_ui, repo, rev=rev)
 
-    def switch_to_date(self, date, directory='.'):
-        hg_ui = ui.ui()
-        repo = hg.repository(hg_ui, directory)
-        commands.update(hg_ui, repo, date=date)
+    def __init__(self, directory='.'):
+        self.ui = ui.ui()
+        self.repo = hg.repository(self.ui, directory)
+
+    def switch_to_revision(self, rev):
+        commands.update(self.ui, self.repo, rev=rev)
+
+    def switch_to_date(self, date):
+        commands.update(self.ui, self.repo, date=date)
 
