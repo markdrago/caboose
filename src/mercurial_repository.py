f205ee5ef586d91506f3486664072840a10a38b0
Mark Drago <mdrago@yodle.com>
2011-03-01 19:29:55 -0500

    get rid of hg_test_utils entirely

--- src/mercurial_repository.py
+++ src/mercurial_repository.py
@@ -1,11 +1,14 @@
+import os
 from mercurial import hg, ui, commands
 
 from repository import Repository
 
 class MercurialRepository(Repository):
 
-    def __init__(self, directory='.'):
+    def __init__(self, directory='.', init=False):
         self.ui = ui.ui()
+        if init and not os.path.isdir(os.path.join(directory, '.hg')):
+            commands.init(self.ui, directory)
         self.repo = hg.repository(self.ui, directory)
 
     def switch_to_revision(self, rev):
@@ -14,3 +17,13 @@ class MercurialRepository(Repository):
     def switch_to_date(self, date):
         commands.update(self.ui, self.repo, date=date)
 
+    def get_working_directory_parent_revision(self):
+        working_directory = self.repo[None]
+        chgset = working_directory.p1()
+        return chgset.rev()
+
+    def get_ui(self):
+        return self.ui
+        
+    def get_repo(self):
+        return self.repo
--- src/test/mercurial_repository_tests.py
+++ src/test/mercurial_repository_tests.py
@@ -1,32 +1,59 @@
 import nose
 from nose.tools import *
 from unittest import TestCase
-from shutil import rmtree
 
+import os
+from shutil import rmtree
+from tempfile import mkdtemp
+from uuid import uuid4
 from mercurial import commands
 
 from mercurial_repository import MercurialRepository
 
-from test_utils import hg_test_utils
-
 class MercurialRepositoryTests(TestCase):
     def setUp(self):
-        (self.repo, self.directory, self.hg_ui) = hg_test_utils.create_repo()
-        self.hgs = MercurialRepository(self.directory)
-
+        self.directory = mkdtemp('-gbtests')
+    
     def tearDown(self):
         rmtree(self.directory)
+
+    @raises(Exception)
+    def test_init_raises_with_init_false_and_non_hg_directory(self):
+        hgrepo = MercurialRepository(self.directory, init=False)
         
+    def test_init_creates_repo_with_init_true_and_non_hg_directory(self):
+        hgrepo = MercurialRepository(self.directory, init=True)
+        ok_(os.path.isdir(os.path.join(self.directory, '.hg')))
+
     def test_switch_to_revision(self):
-        hg_test_utils.create_changesets(self.repo, 3)
-        self.hgs.switch_to_revision(1)
-        repo = hg_test_utils.get_repo_for_directory(self.directory)
-        eq_(1, hg_test_utils.get_working_directory_parent_revision(repo))
+        hgrepo = MercurialRepository(self.directory, init=True)
+        self.create_test_changesets(hgrepo, 3)
+        hgrepo.switch_to_revision(1)
+        eq_(1, hgrepo.get_working_directory_parent_revision())
     
     def test_switch_to_date(self):
+        hgrepo = MercurialRepository(self.directory, init=True)
         dates = ('2011-01-01', '2011-02-02', '2011-03-03')
-        hg_test_utils.create_changesets(self.repo, 3, dates=dates)
-        self.hgs.switch_to_date('2011-02-02')
-        repo = hg_test_utils.get_repo_for_directory(self.directory)
-        eq_(1, hg_test_utils.get_working_directory_parent_revision(repo))
+        self.create_test_changesets(hgrepo, 3, dates=dates)
+        hgrepo.switch_to_date('2011-02-02')
+        eq_(1, hgrepo.get_working_directory_parent_revision())
+
+    @nottest
+    def create_test_changesets(self, repo, count=1, dates=[]):
+        for i in range(count):
+            filename = self._create_random_file()
+            commands.add(repo.get_ui(), repo.get_repo(), filename)
+            
+            date=None
+            if i < len(dates):
+                date=dates[i]
+            commands.commit(repo.get_ui(), repo.get_repo(), date=date, message="creating %s" % (filename,))
+    
+    @nottest        
+    def _create_random_file(self):
+        filename = os.path.join(self.directory, str(uuid4()))
+        with open(filename, 'w') as f:
+            f.write(filename)
+            f.close()
+        return filename
 
