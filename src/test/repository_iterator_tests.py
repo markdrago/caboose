b1a55c823f44ec17775eea1a41beca8137c5711d
Mark Drago <mdrago@yodle.com>
2011-03-03 21:45:11 -0500

    add more tests around repository iterator

--- src/test/repository_iterator_tests.py
+++ src/test/repository_iterator_tests.py
@@ -8,20 +8,31 @@ from repository import Repository
 from repository_iterator import RepositoryIterator
 
 class RepositoryIteratorTests(TestCase):
-    def test_repository_iterator_switches_to_first_revision(self):
-        date_revs = {
+    def setUp(self):
+        self.date_revs = {
             datetime(2011, 1, 1): 0,
-            datetime(2011, 2, 2): 1,
-            datetime(2011, 3, 3): 2
+            datetime(2011, 2, 2): 1
         }
         
-        di = date_revs.keys()
-        di.sort()
-        repo = MockDateRepository(date_revs)
+        self.di = self.date_revs.keys()
+        self.di.sort()
+        self.repo = MockDateRepository(self.date_revs)    
+        self.ri = RepositoryIterator(self.repo, date_iterator=self.di)
 
-        ri = RepositoryIterator(repo, date_iterator=di)
-        ri.next()
-        eq_(repo.latest_rev_requested, 0)
+    def test_repository_iterator_switches_to_first_revision(self):
+        self.ri.next()
+        eq_(self.repo.latest_rev_requested, 0)
+    
+    def test_repository_iterator_switches_to_second_revision(self):
+        self.ri.next()
+        self.ri.next()
+        eq_(self.repo.latest_rev_requested, 1)
+    
+    @raises(StopIteration)
+    def test_repository_iterator_throws_stop_iteration(self):
+        self.ri.next()
+        self.ri.next()
+        self.ri.next()
 
 class MockDateRepository(Repository):
     def __init__(self, date_revs):
