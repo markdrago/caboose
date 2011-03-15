7826790f2f41abd7d50684dfc74cc751e06dedef
Mark Drago <mdrago@yodle.com>
2011-03-15 18:48:30 -0400

    remove useless repository base class - embracing duck typing!

--- src/mercurial_repository.py
+++ src/mercurial_repository.py
@@ -6,7 +6,18 @@ from repository import Repository
 
 dateformat = '%Y-%m-%d %H:%M:%S'
 
-class MercurialRepository(Repository):
+#A repository must implement the following functions:
+#  get_date_of_earliest_commit(self):
+#    returns a datetime object
+#  get_revision_before_date(self, date)
+#    date is a datetime object
+#    returns an object that can be passed to switch_to_revision()
+#  switch_to_revision(self, rev)
+#    rev is an object that was returned from get_revision_before_date()
+#  get_base_directory(self)
+#    returns a string which is the full path to the repository base
+
+class MercurialRepository(object):
     def __init__(self, directory='.', init=False):
         self.ui = ui.ui()
         self.ui.setconfig('ui', 'quiet', True)
