8455f3e8b4d02eb9f5d6dd91dbf5055615807b00
Mark Drago <mdrago@yodle.com>
2011-05-27 08:17:46 -0400

    create multiple stat collectors from config file

--- src/getting_better.py
+++ src/getting_better.py
@@ -3,13 +3,22 @@
 import sys
 
 from config.config_parser import ConfigParser
+from stats.stat_collector_factory import StatCollectorFactory
 
 class GettingBetter(object):
     def __init__(self):
         self.set_config_parser(ConfigParser())
+        self.set_stat_collector_factory(StatCollectorFactory())
 
     def run(self):
-        confobj.run()
+        self.stat_collectors = self.create_stat_collectors(self.config)
+
+    def create_stat_collectors(self, conf):
+        stat_collectors = []
+        for statconf in conf['stats']:
+            sc = self.stat_collector_factory.get_stat_collector(statconf)
+            stat_collectors.append(sc)
+        return stat_collectors
     
     def set_configfile(self, configfile):
         self.configfile = configfile
@@ -17,6 +26,9 @@ class GettingBetter(object):
 
     def set_config_parser(self, parser):
         self.config_parser = parser
+
+    def set_stat_collector_factory(self, scf):
+        self.stat_collector_factory = scf
 
 if __name__ == '__main__':
     gb = GettingBetter()
--- src/test/getting_better_tests.py
+++ src/test/getting_better_tests.py
@@ -6,12 +6,33 @@ from getting_better import GettingBetter
 class GettingBetterTests(TestCase):
     def setUp(self):
         self.gb = GettingBetter()
+
         self.config_parser = MockConfigParser()
         self.gb.set_config_parser(self.config_parser)
+
+        self.stat_collector_factory = MockStatCollectorFactory()
+        self.gb.set_stat_collector_factory(self.stat_collector_factory)
     
-    def test_import_configfile_calls_load_source(self):
+    def test_set_configfile_calls_config_parser(self):
         self.gb.set_configfile('hello/there.conf')
         eq_('hello/there.conf', self.config_parser.last_config_file)
+
+    def test_create_multiple_stat_collectors(self):
+        stat1conf = { 'statname': 'stat1' }
+        stat2conf = { 'statname': 'stat2' }
+        conf = { 'stats' : [ stat1conf, stat2conf ] }
+        scs = self.gb.create_stat_collectors(conf)
+
+        eq_(2, len(self.stat_collector_factory.confs))
+        ok_(stat1conf in self.stat_collector_factory.confs)
+        ok_(stat2conf in self.stat_collector_factory.confs)
+
+class MockStatCollectorFactory(object):
+    def __init__(self):
+        self.confs = []
+
+    def get_stat_collector(self, conf):
+        self.confs.append(conf)
 
 class MockConfigParser(object):
     def __init__(self):
