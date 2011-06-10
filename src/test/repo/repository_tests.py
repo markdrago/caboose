49217b6a72b116fff5cdcdfa4f387070d4fb4d51
Mark Drago <mdrago@yodle.com>
2011-06-10 18:34:21 -0400

    remove broken and unused tests

--- src/test/repo/repository_tests.py
+++ src/test/repo/repository_tests.py
@@ -9,26 +9,6 @@ from repo.repository_factory import RepositoryFactory
 from repo.mercurial_repository import MercurialRepository
 
 class RepositoryTests(TestCase):
-    @raises(Exception)
-    def test_repository_get_revision_before_date_throws(self):
-        r = Repository()
-        r.get_revision_before_date('2011-01-01')
-    
-    @raises(Exception)
-    def test_repository_get_date_of_earliest_commit_throws(self):
-        r = Repository()
-        r.get_date_of_earliest_commit()
-    
-    @raises(Exception)
-    def test_repository_switch_to_revision_throws(self):
-        r = Repository()
-        r.switch_to_revision(0)
-        
-    @raises(Exception)
-    def test_repository_get_base_directory_throws(self):
-        r = Repository()
-        r.get_base_directory()
-    
     def test_get_mercurial_repo_from_factory_for_directory_with_hg_repo(self):
         directory = mkdtemp('-caboose-rep-works-test')
         MercurialRepository(directory, init=True)
