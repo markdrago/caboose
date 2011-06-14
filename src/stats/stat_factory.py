e30e125faff1710ea413e1aa520a0cc5a09467d8
Mark Drago <mdrago@yodle.com>
2011-06-14 18:40:40 -0400

    refactor stat_factory to use classmethod get_name on each stat

--- src/stats/stat_factory.py
+++ src/stats/stat_factory.py
@@ -4,15 +4,13 @@ from stat_java_mean_ccn import StatJavaMeanCcn
 from stat_java_ccn_func_count import StatJavaCcnFuncCount
 
 class StatFactory(object):
+    def __init__(self):
+        self.stat_classes = (StatLines, StatJavaNcss, StatJavaMeanCcn, StatJavaCcnFuncCount)
+
     def get_stat(self, statname):
-        if statname == 'java_ncss':
-            return StatJavaNcss()
-        elif statname == 'lines':
-            return StatLines()
-        elif statname == 'java_mean_ccn':
-            return StatJavaMeanCcn()
-        elif statname == 'java_ccn_func_count':
-            return StatJavaCcnFuncCount()
+        for statclass in self.stat_classes:
+            if statname == statclass.get_name():
+                return statclass()
 
         raise StatDoesNotExistException()
 
