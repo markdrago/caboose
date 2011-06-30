b51e7e5c40361d80e12ca4a04ac509044275243d
Mark Drago <mdrago@yodle.com>
2011-06-30 19:03:38 -0400

    add code which will gather stats about results files to results_stat_collector

--- src/stats/results_stat_collector.py
+++ src/stats/results_stat_collector.py
@@ -7,8 +7,10 @@
 from results.results_package import ResultsPackage
 
 class ResultsStatCollector(object):
-    def __init__(self, stat, results_files):
+    def __init__(self, stat):
         self.stat = stat
+
+    def set_results_files(self, results_files):
         self.results_files = results_files
         self.create_all_results_packages()
 
@@ -28,6 +30,18 @@ class ResultsStatCollector(object):
 
         return rp
 
+    def get_stats(self):
+        rp = ResultsPackage()
+
+        for dt in self.results[0].get_dates():
+            values = [input_rp.get_result(dt) for input_rp in self.results]
+            rp.add_result(dt, self.stat.get_stat(values))
+
+        return rp
+
     def get_results(self):
         return self.results
+
+    def set_results(self, results):
+        self.results = results
 
--- src/test/stats/results_stat_collector_tests.py
+++ src/test/stats/results_stat_collector_tests.py
@@ -10,20 +10,47 @@ from stats.results_stat_collector import ResultsStatCollector
 class ResultsStatCollectorTests(TestCase):
     def test_create_result_packages_from_files(self):
         #write out a json file
-        self.directory = mkdtemp('-caboose-results-stat-collector-tests')
-        filename = path.join(self.directory, 'results.json')
+        directory = mkdtemp('-caboose-results-stat-collector-tests')
+        filename = path.join(directory, 'results.json')
         with file(filename, 'w') as f:
             f.write('{\n  "stats": {\n    "1300489800000": 1234\n  }\n}')
 
         stat = MockResultsStat()
-        results_files = [filename]
-        self.rsc = ResultsStatCollector(stat, results_files)
+        rsc = ResultsStatCollector(stat)
 
-        eq_(1, len(self.rsc.get_results()))
-        eq_(1, self.rsc.get_results()[0].get_date_count())
+        rsc.set_results_files([filename])
 
-        rmtree(self.directory)
+        eq_(1, len(rsc.get_results()))
+        eq_(1, rsc.get_results()[0].get_date_count())
+
+        rmtree(directory)
+
+    def test_get_stat(self):
+        stat = MockResultsStat()
+        rsc = ResultsStatCollector(stat)
+
+        rp1 = MockResultsPackage()
+        rp2 = MockResultsPackage()
+
+        rsc.set_results([rp1, rp2])
+        stats = rsc.get_stats()
+
+        eq_([1, 2, 3], stats.get_dates())
+        eq_(4, stats.get_result(1))
+        eq_(8, stats.get_result(2))
+        eq_(12, stats.get_result(3))
 
 class MockResultsStat(object):
-    pass
+    def set_results_packages(self, results):
+        self.results = results
+
+    def get_stat(self, values):
+        return sum(values)
+
+class MockResultsPackage(object):
+    def get_dates(self):
+        return [1, 2, 3]
+
+    def get_result(self, date):
+        return date * 2
 
