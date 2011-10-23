2029369ff4f981be8ec9ca670f8fd076400e9df8
Mark Drago <mdrago@yodle.com>
2011-10-23 17:34:53 -0400

    pretty much blindly committing work I did over a month ago

--- src/files/file_preprocessor_factory.py
+++ src/files/file_preprocessor_factory.py
@@ -1,8 +1,9 @@
 from file_preprocess_js_subset import FilePreProcessJsSubset
+from file_preprocess_regex import FilePreProcessRegex
 
 class FilePreProcessorFactory(object):
     def get_preprocessor(self, name):
-        classes = (FilePreProcessJsSubset,)
+        classes = (FilePreProcessJsSubset, FilePreProcessRegex)
 
         for clazz in classes:
             if clazz.get_name() == name:
